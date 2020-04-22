c35b5f33bdfa2265ffb3f54c6907c94a78127aa0
kichyr <kichyr@mail.ru>
2020-04-22 21:25:45 +0300

    1

--- app_tests/test_forms/views.py
+++ app_tests/test_forms/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .forms import TestFormForm, StudentTestCase
+import os
+from django.http import HttpResponse
+from app_tests.settings import BASE_DIR
 
 def index_page(request):
     return  JsonResponse({'data': 'test'})
-
 
 def test_create(request):
     if request.method == 'POST':
@@ -20,3 +22,13 @@ def send_test_to_student(request, user_id):
 def get_test_results(request):
     test_results = StudentTestCase.objects.all().values('student_name', 'test_score')
     return JsonResponse({'data': test_results})
+
+# static
+def pass_test(request, id):
+    with open(os.path.join(BASE_DIR, 'static/pass_test', 'index.html')) as file:
+        return HttpResponse(file.read())
+
+# static
+def check_results(request, id):
+    with open(os.path.join(BASE_DIR, 'static/results', 'index.html')) as file:
+        return HttpResponse(file.read())
---
+++ static/results/precache-manifest.22c19c5eb984af564d0a2eed1f03ee1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0748893f61bef69f83b9e4d94593a145",
+    "url": "/index.html"
+  },
+  {
+    "revision": "380f49174874a1c22d85",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a54cd50c58ffac20b898",
+    "url": "/static/js/2.53b55bfc.chunk.js"
+  },
+  {
+    "revision": "b9a68ff2a8681781e77e3c61136482e8",
+    "url": "/static/js/2.53b55bfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "380f49174874a1c22d85",
+    "url": "/static/js/main.e5e9eb74.chunk.js"
+  },
+  {
+    "revision": "0f7e17235ce2106ccee1",
+    "url": "/static/js/runtime-main.549be430.js"
+  }
+]);
