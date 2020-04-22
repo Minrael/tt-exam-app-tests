66e76b82c7430a49fa2408ddd9ab03c29fd792bf
Maria <maria150@mail.ru>
2020-04-22 21:17:59 +0300

    view send_test_to_student fixed

--- app_tests/test_forms/urls.py
+++ app_tests/test_forms/urls.py
@@ -1,10 +1,9 @@
-from .views import index_page, test_create
+from .views import index_page, test_create, send_test_to_student, get_test_results
 from django.urls import path
 
 urlpatterns = [
   path('', index_page, name='index_page'),
   path('test_form', test_create, name='test_create'),
-  path('test_form', test_create, name='test_create'),
-  path('test_form', test_create, name='test_create'),
-  path('test_form', test_create, name='test_create'),
+  path('send_test_to_student/<str:test_name>/<int:user_id>', send_test_to_student, name='send_test_to_student'),
+  path('get_test_results', get_test_results, name='get_test_results'),
 ]
--- app_tests/test_forms/views.py
+++ app_tests/test_forms/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.views.decorators.http import require_GET, require_POST
 from .forms import TestFormForm, StudentTestCase
 
 def index_page(request):
@@ -10,13 +11,14 @@ def test_create(request):
     if request.method == 'POST':
         form = TestFormForm(request.POST)
         if form.is_valid():
-            #test_x = TestForm.objects.create()
             new_test = form.save()
-    return JsonResponse({'data': new_test})
+    return JsonResponse({'new_test': new_test})
 
-def send_test_to_student(request, user_id):
-    return JsonResponse({'data': 'test'})
+def send_test_to_student(request, test_name, user_id):
+    test_instance = TestFormForm.objects.filter(test_name = test_name).values('test_json')
+    return JsonResponse({'test_instance': test_instance, 'test_instance_id': user_id})
 
+@require_GET
 def get_test_results(request):
     test_results = StudentTestCase.objects.all().values('student_name', 'test_score')
     return JsonResponse({'data': test_results})
